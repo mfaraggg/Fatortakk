d78153db335df4e779e1cad18f5cb3230f3e4d72
linah <Linahrashed@aucegypt.edu>
2023-01-20 20:03:32 +0200

    qr updates

---
+++ app/src/main/java/com/example/fatortakk/UserId.java
@@ -0,0 +1,18 @@
+package com.example.fatortakk;
+
+public class UserId {
+    private int id;
+
+    public UserId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
