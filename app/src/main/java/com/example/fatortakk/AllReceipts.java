0f516055a5ab4a3418b7be89a43df3ca778fc116
salmaabdellatif <salmaabdellatif@aucegypt.edu>
2023-01-21 17:45:31 +0200

    Receipt Updates

--- app/src/main/java/com/example/fatortakk/AllReceipts.java
+++ app/src/main/java/com/example/fatortakk/AllReceipts.java
@@ -1,9 +1,15 @@
 package com.example.fatortakk;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,36 +28,55 @@ public class AllReceipts extends AppCompatActivity {
         setContentView(R.layout.activity_all_receipts);
 
         APIInterface myAPI;
-        ListView ReceiptList;
-        String BaseURL = "http://localhost:3000";
+        ListView ReceiptListView = findViewById(R.id.AllReceipts);
+        String BaseURL = "http://10.0.2.2:3000/";
 
-        ReceiptList = findViewById(R.id.AllReceipts);
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BaseURL).addConverterFactory(GsonConverterFactory.create()).build();
         myAPI = retrofit.create(APIInterface.class);
+
+        Resources res = getResources();
+        int [] logoImages = {R.drawable.copa_acai, R.drawable.gourmet, R.drawable.ikea, R.drawable.nike,
+                R.drawable.ovio, R.drawable.starbucks, R.drawable.zara};
+        String[] storeNames = res.getStringArray(R.array.StoresArray);
+
         Call<ArrayList<Receipt>> arrayListCall = myAPI.getReceipts();
+
         arrayListCall.enqueue(new Callback<ArrayList<Receipt>>() {
             @Override
             public void onResponse(Call<ArrayList<Receipt>> call, Response<ArrayList<Receipt>> response) {
-                if (response.isSuccessful())
-                {
-                     ArrayList<Receipt> ReceiptArrayList = new ArrayList<>();
-                     for (int i=0; i<ReceiptArrayList.size(); i++)
-                        {
-                            ReceiptAdapter receiptAdapter = new ReceiptAdapter(ReceiptArrayList, AllReceipts.this, R.layout.singlereceipt);
-                            ReceiptList.setAdapter(receiptAdapter);
-                        }
+                if (response.isSuccessful()) {
+                    Log.d("xxxxxxxxxxx", "RESPONSE SUCCESSFUL");
+                    Log.d("xxxxxxxxxxx", "response body: " + response.body());
+                    ArrayList<Receipt> receiptList = response.body();
+                    if (receiptList != null) {
+                        ReceiptAdapter adapter = new ReceiptAdapter(receiptList, AllReceipts.this, R.layout.singlereceipt,
+                                storeNames,logoImages);
+                        ReceiptListView.setAdapter(adapter);
+                        ReceiptListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                Intent intent = new Intent(AllReceipts.this, ThisReceipt.class);
+                                startActivity(intent);
+                            }
+                        });
+                    } else {
+                        Log.d("xxxxxxxxxxx", "response body is null");
+                        Log.d("xxxxxxxxxxx", "response code: " + response.code());
+                        Log.d("xxxxxxxxxxx", "response message: " + response.message());
+                        Log.d("xxxxxxxxxxx", "response: " + response);
+
+                    }
+                } else {
+                    Log.d("xxxxxxxxxxx", "RESPONSE FAILED");
                 }
             }
 
             @Override
-            public void onFailure(Call<ArrayList<Receipt>> call, Throwable t)
-                {
-                    Toast.makeText(AllReceipts.this, "Failed to load universities.", Toast.LENGTH_SHORT).show();
-                }
+            public void onFailure(Call<ArrayList<Receipt>> call, Throwable t) {
+                Log.d("xxxxxxxxxxx", "RESPONSE FAILED");
+                Log.d("xxxxxxxxxxx", "error: " + t.getMessage());
+                Toast.makeText(AllReceipts.this, "Failed to load receipts.", Toast.LENGTH_LONG).show();
+            }
         });
-
-
-
-
     }
 }
--- app/src/main/java/com/example/fatortakk/ReceiptAdapter.java
+++ app/src/main/java/com/example/fatortakk/ReceiptAdapter.java
@@ -1,10 +1,13 @@
 package com.example.fatortakk;
 
+
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,12 +18,16 @@ public class ReceiptAdapter extends BaseAdapter {
     private ArrayList<Receipt> ReceiptList;
     private Context context;
     private int Layout;
+    String[] StoreNames;
+    int[] logoImages;
 
-    public ReceiptAdapter(ArrayList<Receipt> ReceiptList, Context context, int Layout)
+    public ReceiptAdapter(ArrayList<Receipt> ReceiptList, Context context, int Layout, String[] StoreNames, int[] logoImages)
     {
         this.ReceiptList = ReceiptList;
         this.context = context;
         this.Layout = Layout;
+        this.StoreNames = StoreNames;
+        this.logoImages = logoImages;
     }
 
     @Override
@@ -41,6 +48,7 @@ public class ReceiptAdapter extends BaseAdapter {
     private class ViewHolder
     {
         TextView StoreTxt, DateTxt, TimeTxt, TotalTxt;
+        ImageView Logo;
     }
     @Override
     public View getView(int pos, View convertView, ViewGroup parent) {
@@ -51,12 +59,27 @@ public class ReceiptAdapter extends BaseAdapter {
         viewHolder.DateTxt = convertView.findViewById(R.id.DateTxt);
         viewHolder.TimeTxt = convertView.findViewById(R.id.TimeTxt);
         viewHolder.TotalTxt = convertView.findViewById(R.id.TotalTxt);
+        viewHolder.Logo = convertView.findViewById(R.id.Logos);
 
         Receipt receipt = ReceiptList.get(pos);
         viewHolder.StoreTxt.setText(receipt.getName());
         viewHolder.DateTxt.setText(receipt.getDate());
         viewHolder.TimeTxt.setText(receipt.getTime());
         viewHolder.TotalTxt.setText(String.valueOf(receipt.getTotal()));
+
+        boolean isLogoFound = false;
+        for (int i = 0; i < StoreNames.length; i++) {
+            if (receipt.getName().equals(StoreNames[i])) {
+                // Set the logo image
+                viewHolder.Logo.setImageResource(logoImages[i]);
+                isLogoFound = true;
+                break;
+            }
+        }
+        if (!isLogoFound) {
+            viewHolder.Logo.setImageResource(R.drawable.default_logo);
+        }
+
 
         return convertView;
     }
